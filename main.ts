336729b6df226f727b2bbc8a7f4256816dd11899
BetzElisabeth <74718590+BetzElisabeth@users.noreply.github.com>
2020-11-19 13:17:56 +0000

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,26 @@
+radio.setGroup(3)
+let Spielphase = "warten"
+let Schwierigkeit = 1
+let Platzhalter = input.acceleration(Dimension.X)
+basic.forever(function () {
+    if (true) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Spielphase == "spielen") {
+    	
+    } else {
+    	
+    }
+})
